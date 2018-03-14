8f1eea5541c45e970c483fd8a782149687e4355c
mcstarioni <mcstarioni@live.ru>
2018-03-14 18:01:55 +0300

    assignment2

--- src/lab2/Assignment1.java
+++ src/lab2/Assignment1.java
@@ -11,18 +11,68 @@ public class Assignment1
     private static final String user = "trpo";
     private static final String password = "trpo";
     private static Connection connection;
-    public static void main(String[] args) throws Exception
+    private static ArrayList<String> fieldNames = new ArrayList<>();
+
+    public static class Part1
     {
-        Gson gson = new Gson();
-        String jsonFile = "";
-        FileInputStream fi = new FileInputStream(
-                "C:\\Users\\Макс\\Documents\\Programming\\" +
-                "6 semester\\TRPO\\src\\lab2\\E05_aanderaa_all_1769_d432_5004.json");
-        jsonFile = readFromJson(fi);
-        JsonObject table = gson.fromJson(jsonFile, JsonObject.class).get("table").getAsJsonObject();
-        final ArrayList<JsonArray> fields = new ArrayList<>();
-        table.keySet().forEach((String s) -> fields.add(table.get(s).getAsJsonArray()));
-        writeToDatabase(fields);
+        public static void main(String[] args) throws Exception
+        {
+            Gson gson = new Gson();
+            String jsonFile = "";
+            FileInputStream fi = new FileInputStream(
+                    "C:\\Users\\Макс\\Documents\\Programming\\" +
+                            "6 semester\\TRPO\\src\\lab2\\E05_aanderaa_all_1769_d432_5004.json");
+            jsonFile = readFromJson(fi);
+            JsonObject table = gson.fromJson(jsonFile, JsonObject.class).get("table").getAsJsonObject();
+            final ArrayList<JsonArray> fields = new ArrayList<>();
+            table.keySet().forEach((String s) -> fields.add(table.get(s).getAsJsonArray()));
+            writeToDatabase(fields);
+        }
+    }
+    public static class Part2
+    {
+        public static void main(String[] args) throws Exception
+        {
+            Map<String,Map> jsonTable = new HashMap<>();
+
+            Connection con = DriverManager.getConnection(url, user, password);
+            Statement stm = con.createStatement();
+            ResultSet columns = stm.executeQuery("SHOW COLUMNS FROM Weather_station;");
+            int i = 1;
+            while(columns.next())
+            {
+                fieldNames.add(columns.getString(1));
+            }
+            for (String name:fieldNames)
+            {
+                if(name.endsWith("_qc"))
+                {
+                    continue;
+                }
+
+                String clause = (fieldNames.contains(name + "_qc")?("WHERE "+name+"_qc = 0"):"");
+                ResultSet values = con.createStatement().executeQuery(
+                        "SELECT (SELECT MIN(time) FROM Weather_station) AS start_date, (SELECT MAX(time) FROM Weather_station) AS end_date,\n" +
+                                "count(id) AS num_records,\n" +
+                                "MIN("+name+") AS min_"+name+",\n" +
+                                "MIN(time) AS min_time,\n" +
+                                "MAX("+name+") AS max_"+name+",\n" +
+                                "MAX(time) AS max_time,\n" +
+                                "AVG("+name+") AS avg_"+name+"\n" +
+                                "FROM Weather_station " + clause +";");
+                while (values.next())
+                {
+                    Map<String,Object> valuesTable = new HashMap<>();
+                    ResultSetMetaData meta = values.getMetaData();
+                    for (int j = 1; j < meta.getColumnCount(); j++)
+                    {
+                        valuesTable.put(meta.getColumnName(j),values.getObject(j));
+                    }
+                    jsonTable.put(name,valuesTable);
+                }
+            }
+            System.out.println(new Gson().toJson(jsonTable));
+        }
     }
     private static String readFromJson(InputStream inputStream)
     {
@@ -62,7 +112,9 @@ public class Assignment1
         fields.get(1).iterator().forEachRemaining(
             element ->
             {
-                attributes.append(iter.next().getAsString())
+                String fieldName = iter.next().getAsString();
+                fieldNames.add(fieldName);
+                attributes.append(fieldName)
                         .append(" ")
                         .append(typesToDatabase.get(element.getAsString()))
                         .append(" not null,\n ");
